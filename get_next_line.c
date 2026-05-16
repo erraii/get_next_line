ab3f21e623fcdb4abb8e47ab56d89055487a4c87
erraii <eraycakiray@gmail.com>
2026-05-17 00:01:43 +0200

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -11,165 +11,209 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-static char	*ft_strdup(const char *s)
-{
-	int		str_len;
-	char	*mem_copied;
-
-	str_len = 0;
-	while (s[str_len])
-		str_len++;
-	mem_copied = malloc((str_len + 1) * sizeof(char));
-	if (!mem_copied)
-		return (0);
-	str_len = 0;
-	while (s[str_len])
-	{
-		mem_copied[str_len] = s[str_len];
-		str_len++;
-	}
-	mem_copied[str_len] = '\0';
-	return (mem_copied);
-}
-
-static char	*until_nl(char *s, size_t *len)
+size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
-	*len = 0;
 	i = 0;
-	while (s[i] != '\0')
+	while (s[i])
+		i ++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = 0;
+	while (*(src + src_len))
+		src_len++;
+	if (size == 0)
+		return (src_len);
+	i = 0;
+	while (*(src + i) && (i < (size - 1)))
 	{
-		if (s[i] == '\n')
-		{
-			*len = i + 1;
-			return (ft_substr(s, 0, *len));
-		}
+		dst[i] = src[i];
 		i++;
 	}
-	return (NULL);
+	dst[i] = '\0';
+	return (src_len);
+}
+
+static char	*till_nl(const char *s, char **rem)
+{
+	size_t	i;
+	char	*res;
+
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (s[i] == '\n')
+		i++;
+	else
+		i = 0;
+	res = malloc(i + 1);
+	if (!res)
+		return (NULL);
+	*rem = malloc(ft_strlen(s + i) + 1);
+	if (!*rem)
+	{
+		free(res);
+		return (NULL);
+	}
+	ft_strlcpy(res, s, i + 1);
+	ft_strlcpy(*rem, s + i, ft_strlen(s + i) + 1);
+	return (res);
 }
 
 char	*get_next_line(int fd)
 {
-	t_getline	s_line;
-	static char	*left_over;
+	t_gl	s_line;
+	static char	remain[BUFFER_SIZE + 1];
 
 	s_line.line = NULL;
+	s_line.tmp = NULL;
 	s_line.buffer = NULL;
-	s_line.tmp_str = NULL;
-	s_line.nl_len = 0;
-	s_line.lo_len = 0;
-	while (s_line.nl_len == 0)
+	s_line.nl_found = 0;
+	if (*remain)
 	{
-		if (left_over)
+		//printf("remainde line aranıyor\n");
+		s_line.line = till_nl(remain, &s_line.tmp);
+		if (!s_line.line || !s_line.tmp)
 		{
-			s_line.line = until_nl(left_over, &s_line.nl_len);
-			if (s_line.line)
-			{
-				s_line.lo_len = ft_strlen(left_over) - s_line.nl_len;
-				s_line.tmp_str = ft_substr(left_over, s_line.nl_len, s_line.lo_len);
-				if (!s_line.tmp_str)
-					return (NULL);
-				free(left_over);
-				left_over = NULL;
-				if (*s_line.tmp_str)
-					left_over = ft_strdup(s_line.tmp_str);
-				free(s_line.tmp_str);
-				return (s_line.line);
-			}
 			free(s_line.line);
-			s_line.line = NULL;
-		}
-
-		s_line.buffer = malloc(BUFFER_SIZE + 1);
-		if (!s_line.buffer)
+			free(s_line.tmp);
 			return (NULL);
-		s_line.bytesread = read(fd, s_line.buffer, BUFFER_SIZE);
-		if (s_line.bytesread == 0)
-		{
-			s_line.nl_len = 1;
-			free (s_line.buffer);
-			if (left_over && *left_over)
-				s_line.line = ft_strdup(left_over);
-			free (left_over);
-			left_over = NULL;
-			return (s_line.line);
 		}
-		else if (s_line.bytesread > 0)
+		if (*s_line.line)
 		{
-			s_line.buffer[BUFFER_SIZE] = '\0';
-			s_line.tmp_str = until_nl(s_line.buffer, &s_line.nl_len);
-			if (s_line.tmp_str)
+			ft_strlcpy(remain, s_line.tmp, ft_strlen(s_line.tmp) + 1);
+			free(s_line.tmp);
+			return(s_line.line);
+		}
+		free(s_line.line);
+		free(s_line.tmp);
+		s_line.line = malloc(ft_strlen(remain) + 1);
+		if (!s_line.line)
+			return (NULL);
+		ft_strlcpy(s_line.line, remain, ft_strlen(remain) + 1);
+		remain[0] = '\0';
+	}
+	//printf("remainde line yok\n");
+	while (s_line.nl_found == 0)
+	{
+		s_line.b_read = read(fd, remain, BUFFER_SIZE);
+		if (s_line.b_read == 0)
+		{
+			remain[0] = '\0';
+			s_line.nl_found = 1;
+		}
+		else if (s_line.b_read > 0)
+		{
+			remain[s_line.b_read] = '\0';
+			//printf("okundu: %s\n", remain);
+			s_line.buffer = till_nl(remain, &s_line.tmp);
+			remain[0] = '\0';
+			if (!s_line.buffer || !s_line.tmp)
 			{
-				if (left_over)
-				{
-					s_line.lo_len = ft_strlen(s_line.buffer) - s_line.nl_len + ft_strlen(left_over);
-					s_line.line = ft_strjoin(left_over, s_line.tmp_str);
-					free (s_line.tmp_str);
-					s_line.tmp_str = ft_substr(s_line.buffer, s_line.nl_len, s_line.lo_len);
-					free(left_over);
-					left_over = ft_strdup(s_line.tmp_str);
-					free (s_line.tmp_str);
-					free (s_line.buffer);
-				}
-				else
-				{
-					s_line.lo_len = ft_strlen(s_line.buffer) - s_line.nl_len;
-					s_line.line = ft_strdup(s_line.tmp_str);
-					free (s_line.tmp_str);
-					s_line.tmp_str = ft_substr(s_line.buffer, s_line.nl_len, s_line.lo_len);
-					left_over = ft_strdup(s_line.tmp_str);
-					free (s_line.tmp_str);
-					free (s_line.buffer);
-				}
+				free(s_line.buffer);
+				free(s_line.tmp);
+				return (NULL);
 			}
-			else
+			if (*s_line.buffer)
 			{
-				if (!left_over)
+				//printf("döngü: remainde line bulundu\n");
+				//printf("buffer: %s\n", s_line.buffer);
+				//printf("remain: %s\n", s_line.tmp);
+				ft_strlcpy(remain, s_line.tmp, ft_strlen(s_line.tmp) + 1);
+				free(s_line.tmp);
+				if (s_line.line)
 				{
-					left_over = ft_strdup(s_line.buffer);
+					s_line.tmp = malloc(ft_strlen(s_line.line) + 1);
+					if(!s_line.tmp)
+						return(NULL);
+					ft_strlcpy(s_line.tmp, s_line.line, ft_strlen(s_line.line) + 1);
+					free(s_line.line);
+					s_line.line = malloc(ft_strlen(s_line.tmp) + ft_strlen(s_line.buffer) + 1);
+					if(!s_line.line)
+						return(NULL);
+					ft_strlcpy(s_line.line, s_line.tmp, ft_strlen(s_line.tmp) + 1);
+					free(s_line.tmp);
+					ft_strlcpy(s_line.line + ft_strlen(s_line.line), s_line.buffer, ft_strlen(s_line.buffer) + 1);
 					free(s_line.buffer);
 				}
 				else
 				{
-					s_line.tmp_str = ft_strdup(left_over);
-					free(left_over);
-					left_over = NULL;
-					left_over = ft_strjoin(s_line.tmp_str, s_line.buffer);
-					free (s_line.tmp_str);
-					free (s_line.buffer);
+					//printf("döngü: remainde line var, s_line yok\n");
+					s_line.line = malloc(ft_strlen(s_line.buffer) + 1);
+					if(!s_line.line)
+						return(NULL);
+					ft_strlcpy(s_line.line, s_line.buffer, ft_strlen(s_line.buffer) + 1);
+					free(s_line.buffer);
+				}
+				s_line.nl_found = 1;
+			}
+			else
+			{
+				//printf("döngü: remainde line yok\n");
+				free(s_line.buffer);
+				if (s_line.line)
+				{
+					s_line.buffer = malloc(ft_strlen(s_line.line) + 1);
+					ft_strlcpy(s_line.buffer, s_line.line, ft_strlen(s_line.line) + 1);
+					free(s_line.line);
+					s_line.line = malloc(ft_strlen(s_line.buffer) + ft_strlen(s_line.tmp) + 1);
+					ft_strlcpy(s_line.line, s_line.buffer, ft_strlen(s_line.buffer) + 1);
+					free(s_line.buffer);
+					ft_strlcpy(s_line.line + ft_strlen(s_line.line), s_line.tmp, ft_strlen(s_line.tmp) + 1);
+					free(s_line.tmp);
+				}
+				else
+				{
+					s_line.line = malloc(ft_strlen(s_line.tmp) + 1);
+					ft_strlcpy(s_line.line, s_line.tmp, ft_strlen(s_line.tmp) + 1);
+					free(s_line.tmp);
 				}
 			}
 		}
 		else
 		{
-			free (s_line.buffer);
-			return(NULL);
+			remain[0] = '\0';
+			return (NULL);
 		}
+		//free(s_line.buffer);
 	}
-	return (s_line.line);
+	return(s_line.line);
 }
 
-// int	main(void)
-// {
-// 	int		fd;
-// 	char	*line;
+int	main(void)
+{
+	int		fd;
+	char	*line;
+/* 	char	*remain;
+	char	*s; */
 
-// 	fd = open("1char.txt", O_RDONLY);
-// 	if (fd == -1)
-// 		return (1);
-// 	for (int i = 0; i < 1; i++)
-// 	{
-// 		line = get_next_line(fd);
-// 		printf("line[%d]: %s", i + 1, line);
-// 		free (line);
-// 		line = NULL;
-// 	}
-// 	close(fd);
-// }
+/* 	s = "abc\ndef";
+	line = till_nl(s, &remain);
+	printf("line: %s", line);
+	printf("remain: %s", remain);
+	free(line);
+	free(remain); */
+	fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+		return (1);
+	for (int i = 0; i < 52; i++)
+	{
+		line = get_next_line(fd);
+		if(line)
+			printf("line[%d]: %s", i + 1, line);
+		free (line);
+		line = NULL;
+	}
+	close(fd);
+}
